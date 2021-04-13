feee3098ce8d42ff5b612b8fbe7bd13a2d7099f6
Ish Mahajan <ishmahajanbhanu@gmail.com>
2021-04-13 23:02:49 +0530

    initial commit

---
+++ src/main/java/com/example/spring/boot/composite/SpringBootCompositeApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.composite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCompositeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCompositeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/spring/boot/composite/domain/insurance/Insurance.java
@@ -0,0 +1,31 @@
+package com.example.spring.boot.composite.domain.insurance;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@EqualsAndHashCode
+@Entity
+public class Insurance implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "ID", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    private String insuredBy;
+
+    private LocalDate insuredOn;
+
+    private transient  String formattedDate;
+
+}
